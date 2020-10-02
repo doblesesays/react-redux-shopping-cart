ffa4fe1af0b26fe0b7b7ead663b2239dc54e01ea
Genessis Jimenez <genesis.jz.93@gmail.com>
2020-10-02 15:51:50 -0400

    fix on cart component

--- src/App.js
+++ src/App.js
@@ -46,8 +46,8 @@ class App extends React.Component {
     this.setState(state => ({
       sort,
       products: this.state.products.slice().sort((a,b) => (
-        sort == "lowest" ? ((a.price > b.price) ? 1 : -1) :
-        sort == "highest" ? ((a.price < b.price) ? 1 : -1) :
+        sort === "lowest" ? ((a.price > b.price) ? 1 : -1) :
+        sort === "highest" ? ((a.price < b.price) ? 1 : -1) :
         ((a._id < b._id) ? 1 : -1)
       ))
     }))
